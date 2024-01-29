312c37f8c5b1cc8f6e2a81b1b58208e680dcde49
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-01-29 20:16:53 -0300

    first commit

---
+++ src/test/java/com/patrick/social_network/SocialNetworkMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrick.social_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
