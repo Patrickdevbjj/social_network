1685e65fe053e6002fb7e6fad942bd013c5ca411
Patrick Rodrigues <patrickdev6@hotmail.com>
2024-02-07 17:32:48 -0300

    add POST method

--- src/main/java/com/patrick/social_network/service/UserService.java
+++ src/main/java/com/patrick/social_network/service/UserService.java
@@ -1,6 +1,7 @@
 package com.patrick.social_network.service;
 
 import com.patrick.social_network.domain.User;
+import com.patrick.social_network.dto.UserDTO;
 import com.patrick.social_network.repository.UserRepository;
 import com.patrick.social_network.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
